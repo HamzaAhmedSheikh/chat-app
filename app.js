45e9d7c07cf006241198ad259df17d4d63dcceb1
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-17 21:53:27 +0500

    design is complete

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log('Hello Chat App');
 
+ function StartChat(id) {
+     document.getElementById('chatPanel').removeAttribute('style');
+     document.getElementById('divStart').setAttribute('style', 'display:none');
+ }
