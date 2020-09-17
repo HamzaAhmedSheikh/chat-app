794ca3ab1be06dc161ea0836a7c10e0e7e7b72e1
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-18 00:33:35 +0500

    add responsiveness

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ console.log('Hello Chat App');
  function StartChat(id) {
      document.getElementById('chatPanel').removeAttribute('style');
      document.getElementById('divStart').setAttribute('style', 'display:none');
+
+     hideChatList()
+ }
+
+ ////////////////////////////////
+
+ function showChatList() {
+    document.getElementById('side-1').classList.remove('d-none', 'd-md-block');    
+    document.getElementById('side-2').classList.add('d-none');
+ }
+
+ function hideChatList() {
+    document.getElementById('side-1').classList.add('d-none', 'd-md-block');    
+    document.getElementById('side-2').classList.remove('d-none');
  }
