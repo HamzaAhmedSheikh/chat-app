7d340ae7e38577512fef7135afa5ba8e393ee94d
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-18 02:03:02 +0500

    sent messages

--- app.js
+++ app.js
@@ -18,3 +18,39 @@ console.log('Hello Chat App');
     document.getElementById('side-1').classList.add('d-none', 'd-md-block');    
     document.getElementById('side-2').classList.remove('d-none');
  }
+
+ ///////////////////////////////////////////
+
+ function OnKeyDown() {
+     document.addEventListener('keydown', function(key) {
+         if(key.key === "Enter") {
+             SendMessage()            
+         }
+         
+     });     
+ }
+
+ function SendMessage() {
+     let message = `<div class="row justify-content-end">              
+                     <div class="col-6 col-sm-7 col-md-7">
+                      <p class="sent float-right"> 
+                        ${document.getElementById('txtMessage').value}                      
+                       <span class="time float-right"> 1:28 PM </span>
+                      </p>
+                     </div>
+                     
+                     <div class="col-2 col-sm-1 col-md-1">
+                      <img src="./images/pp.png" class="chat-pic" alt="profile-pic" />
+                     </div>  
+                    </div>`
+
+        document.getElementById('messages').innerHTML += message;
+        document.getElementById('txtMessage').value = '';   
+        document.getElementById('txtMessage').focus();       
+
+        document.getElementById('messages').scrollTo(0, document.getElementById('messages').clientHeight);
+ }
+
+ /////////////////////////////////////////////////
+
+ 
---
+++ firebase.init.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDccDXNTtRjAcDTvvF3TvAkReQ_Hgz9Szc",
+    authDomain: "chat-app-f3aa1.firebaseapp.com",
+    databaseURL: "https://chat-app-f3aa1.firebaseio.com",
+    projectId: "chat-app-f3aa1",
+    storageBucket: "chat-app-f3aa1.appspot.com",
+    messagingSenderId: "173602540884",
+    appId: "1:173602540884:web:3916a0f88fdb286a800cc1",
+    measurementId: "G-1MHJ5ET675"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
